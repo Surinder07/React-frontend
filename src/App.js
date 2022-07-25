c6ac44877f394c44be81f0e0634ed8f7973cf2b1
Surinder Singh <Surinder0322@gmail.com>
2022-07-24 21:13:27 -0400

    added HeaderComponent

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import HeaderComponent from './components/HeaderComponent';
 import ListEmployeeComponent from './components/ListEmployeeComponent';
 
 function App() {
   return (
     <div className="App">
+       <HeaderComponent/>
        <ListEmployeeComponent/>
     </div>
   );
